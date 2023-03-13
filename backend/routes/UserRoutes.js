d8ff7f2211e3cfb81b52761ac003a96cc6feb065
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-13 14:57:25 -0300

    Criação de models e controller de usuario

---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const { register } = require("../controllers/UserController");
+
+const validate = require("../middlewares/handleValidation");
+
+router.post("/register", validate, register);
+
+module.exports = router;
