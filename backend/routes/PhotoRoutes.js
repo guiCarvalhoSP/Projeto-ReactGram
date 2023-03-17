5c1f867c8438cfa528bf95562c92136f8b55680d
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-17 14:07:23 -0300

    Função de adição de fotos

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const { insertPhoto } = require('../controllers/PhotoControler');
+
 const { photoInsertValidation } = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
+const { imageUpload } = require('../middlewares/imageUpload');
+
+router.post(
+  "/",
+  authGuard,
+  imageUpload.single("image"),
+  photoInsertValidation(),
+  validate,
+  insertPhoto
+);
 
 module.exports = router;
