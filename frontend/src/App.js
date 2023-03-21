669c90a6457111133bb7d214312254f1b5f92697
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-21 10:12:26 -0300

    Criação do navbar e footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from "./pages/Home/Home"
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/register' element={<Register />}/>
-
-        </Routes>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Register />}/>
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
-      <h1>ReactGram</h1>
     </div>
   );
 }
