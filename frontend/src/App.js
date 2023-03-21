64ffa6edfb639c4b121d1f86e478c9d763f2f973
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-21 09:51:55 -0300

    Paginas iniciais

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Home from "./pages/Home/Home"
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+
+        </Routes>
+      </BrowserRouter>
       <h1>ReactGram</h1>
     </div>
   );
