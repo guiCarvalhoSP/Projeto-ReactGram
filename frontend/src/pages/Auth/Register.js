d7b824b68a8cd396198e9ab799dda65a348c636b
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-21 10:59:06 -0300

    Estilos globais
    Form de registro de usuário

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
 import "./Auth.css";
+
+import { useState, useEffect } from "react";
+
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text"  placeholder="Nome"/>
+        <input type="email"  placeholder="E-mail"/>
+        <input type="password"  placeholder="Senha"/>
+        <input type="password"  placeholder="Confirme a senha"/>
+        <input type="submit"  value="Cadastrar"/>
+      </form>
+
+      <p>Já possuí uma conta? <Link to="/login">Clique e faça o login!</Link></p>
+    </div>
   )
 }
 
